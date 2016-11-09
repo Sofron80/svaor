1a36526f07341b8c5cd278020d1d9a7f4dd48d94
sofron80 <sofron80@mail.ru>
2016-11-09 15:36:16 +0600

    0.0.2

--- site/core/urls.py
+++ site/core/urls.py
@@ -16,21 +16,21 @@ Including another URLconf
 from django.conf.urls import url
 from django.contrib import admin
 
-from company.views import index, error, contact, company, catalog, category, tovar, custom_500
+from company.views import index, contact, company, catalog, category
 
 
 urlpatterns = [
     url(r'^$', index, name='index'),
-    url(r'^er/$', error),
+   
     url(r'^admin/', admin.site.urls),
     url(r'^contacts/$', contact, name="contacts" ),
     url(r'^o-kompanii/$', company, name="company" ),
     url(r'^catalog/$', catalog, name="catalog" ),
     # url(r'^category/$', catalog, name="category" ),
     url(r'^category/(?P<cat_slug>[\w-]+)/$', category, name='category'),
-    url(r'^category/(?P<cat_slug>[\w-]+)/(?P<tov_slug>[\w-]+)/$', tovar),
+   # url(r'^category/(?P<cat_slug>[\w-]+)/(?P<tov_slug>[\w-]+)/$', tovar),
 ]
-handler500 = custom_500
+#handler500 = custom_500
 
 from django.conf import settings
 from django.conf.urls.static import static
