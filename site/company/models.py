0a4193a5585c940115a69b2b880b5dff330f27b6
sofron80 <sofron80@mail.ru>
2016-11-09 15:53:51 +0600

    0.0.3

--- site/company/models.py
+++ site/company/models.py
@@ -1,7 +1,5 @@
-
 from django.db import models
 # from django.conf import settings
-
 
 class Category(models.Model):
     name = models.CharField(max_length=250, verbose_name="Наименование категории")
@@ -19,7 +17,6 @@ class Category(models.Model):
         verbose_name = "Категория"
         verbose_name_plural = "Категории"
 
-
 def upload_to(instance, filename):
     return "{}/{}/{}".format(instance.category.slug, instance.slug, filename)
 
@@ -28,12 +25,12 @@ class Tovar(models.Model):
     slug = models.CharField(max_length=250, verbose_name ="Наименование для ссылки" )
     keuw = models.CharField(max_length=250, verbose_name="Ключевые слова", blank = True, null=True)
     desk = models.CharField(max_length=250, verbose_name="Краткое описание", blank = True, null=True)
-    
     cena = models.FloatField( blank = True, null=True, verbose_name="Цена" )
     ed = models.CharField(max_length=50, verbose_name="Единицы измерения", blank = True, null=True)
     dostavka = models.CharField(max_length=100, blank = True, null=True, verbose_name="Доставка")
     img = models.ImageField(upload_to=upload_to, blank = True, null=True)
     category = models.ForeignKey(Category)
+
     def get_url(self):
         return "/category/{}/{}/".format(self.category.slug, self.slug)
 
@@ -56,7 +53,7 @@ class Page(models.Model):
         if self.slug == "index":
             return "/"
         return "/{}/".format( self.slug)
-        
+
     class Meta():
         verbose_name = "Страница"
         verbose_name_plural = "Страницы"
